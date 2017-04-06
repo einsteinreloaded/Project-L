f7d9e119c422f55a2f2d7caa04c7106e537a6a16
yadhu <yadhu@qburst.com>
2017-04-06 16:49:55 +0530

    more and more things

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -2,23 +2,41 @@ const webpack = require('webpack');
 const path = require('path');
 
 module.exports = {
-  entry: './src/app/app.jsx',
+  entry: './src/client.js',
   output: {
+    // build files into a real folder
     path: path.resolve('dist'),
-    filename: 'bundle.js'
-  },
-  plugins: [
-    new webpack.ProvidePlugin({
-      "React": "react",
-    }),
-  ],
-  externals: {
-    'react': 'React'
+    filename: 'bundle.js',
+    // so the build files (in memory) are served from here -> localhost:8080/dist/*
+    publicPath: '/dist/',
   },
   module: {
     loaders: [
-      { test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/ },
-      { test: /\.jsx$/, loader: 'babel-loader', exclude: /node_modules/ }
+      // loads your js OR jsx
+      { test: /\.jsx?$/, loader: 'babel-loader', exclude: /node_modules/ },
+      // load scss files
+      {
+        test: /\.scss$/,
+        loaders: [
+          'style-loader',
+          // builds css modules' classnames from the path of the component in the tree.
+          // eg: "src-components-Header-styles-header"
+          // so it would be easy for debugging (we can minifiy these classnames in prod mode)
+          'css-loader?importLoaders=2&module&localIdentName=[path][name]-[local]&-autoprefixer&-minimize',
+          'sass-loader',
+        ],
+      },
     ]
+  },
+  resolve: {
+    modules: [
+      // so you can simple import your components as:
+      // - import Header from 'components/Header' OR
+      // - import App from 'containers/App'
+      // any files under src/ folder can be imported as above anwhere under /src (no matter how deep you're).
+      // absolute paths FTW!
+      'src',
+      'node_modules',
+    ],
   }
 };
---
+++ src/containers/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Header from 'components/Header'
+
+/**
+ * Will be useful to supply some common data that needs to be available for all components.
+ * eg: i18n and config.
+ * You can supply those using React 'context'
+ * Read it here https://facebook.github.io/react/docs/context.html
+ * 
+ * Also sometime we will use Redux 'connect' to supply some dat to App.
+ * This is the reason why I have put App inside 'containers'.
+ */
+
+const App = () => (
+  <Header />
+);
+
+export default App;
