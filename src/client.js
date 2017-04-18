0a83a872ef0c15bd9d24eee9a46ece549a81fd6a
Anish Kumar <changayil1994@gmail.com>
2017-04-18 13:31:00 +0530

    Implementing react router redux beta phase

--- src/client.js
+++ src/client.js
@@ -2,18 +2,31 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from 'containers/App';
 import $ from 'jquery';
-import { createStore } from 'redux';
+import { createStore, combineReducers } from 'redux';
 import { Provider } from 'react-redux';
-import reducer from './reducers/login';
+import { Router, Route, browserHistory } from 'react-router'
+import { syncHistoryWithStore, routerReducer } from 'react-router-redux'
+import reducers from './reducers/login';
 /**
  * React router will wrap your App in the future
  * Redux will supply store to your App
  */
-const store = createStore(reducer);
+// Add the reducer to your store on the `routing` key
+const store = createStore(
+  combineReducers({
+    ...reducers,
+    routing: routerReducer
+  })
+)
+
+// Create an enhanced history that syncs navigation events with the store
+const history = syncHistoryWithStore(browserHistory, store)
 
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <Router history={browserHistory}>
+        <Route path="/" component={App} />
+    </Router>
   </Provider>,
   document.getElementById('lapp')
 );
