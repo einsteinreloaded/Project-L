3012ec18a99252516d30c5ec3ee21b0ac2782331
Anish Kumar <changayil1994@gmail.com>
2017-04-25 10:56:26 +0530

    trying ref approach in loginform

--- src/components/Login/LoginForm/index.js
+++ src/components/Login/LoginForm/index.js
@@ -7,31 +7,17 @@ import LoginServices from 'services/LoginService';
 class LoginForm extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {username: '', password:'', isloggedin: false, user: ''};
-
-    this.handleUserNameChange = this.handleUserNameChange.bind(this);
-    this.handlePasswordChange = this.handlePasswordChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
-  }
-
-  handleUserNameChange(event) {
-    this.setState({username: event.target.value});
-  }
-   handlePasswordChange(event) {
-    this.setState({password: event.target.value});
   }
 
   handleSubmit(event) {
     event.preventDefault();
-    LoginServices.getAuthentication(this.state.username,this.state.password).then((res)=>{
+    LoginServices.getAuthentication(this.username.value,this.password.value).then((res)=>{
         console.log(res.data);
-        //if(data.authTokenId){
-        this.props.login({...this.state,id: res.data.authTokenId, user: res.data.firstName});
-        //this.props.dispatch(login({...this.state,id: res.data.authTokenId, user: res.data.firstName}));
+        this.props.login({id: res.data.authTokenId, user: res.data.firstName});
     }).catch((err)=>{
         console.log(err);
     });
-    
   }
 
   render() {
@@ -44,16 +30,16 @@ class LoginForm extends React.Component {
     }
     return (
       <form onSubmit={this.handleSubmit}>
-          <div className="form-group">
-            <label htmlFor="uname">
-            UserName:
-            <input type="text" name="uname" value={this.state.username} className="form-control" onChange={this.handleUserNameChange} />
-            </label>
+        <div className="form-group">
+          <label htmlFor="uname">
+          UserName:
+          <input type="text" name="uname" className="form-control" ref={(input) => this.username = input} />
+          </label>
         </div>
         <div className="form-group">
             <label htmlFor="pword">
             Password:
-            <input type="password" name="pword" className="form-control" value={this.state.password} onChange={this.handlePasswordChange} />
+            <input type="password" name="pword" className="form-control" ref={(input) => this.password = input} />
             </label>
         </div>
         <input type="submit" value="Submit" />
